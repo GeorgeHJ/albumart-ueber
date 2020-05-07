4152db249fc3d27508cafd885cb39eeaef2512fd
GeorgeHJ <geh-jpro@hotmail.co.uk>
2020-05-07 18:40:26 +0100

    Move functions into a logical order

--- albumart-ueber.bash
+++ albumart-ueber.bash
@@ -11,6 +11,25 @@ y=0
 # shellcheck disable=SC1090
 source "$(ueberzug library)"
 
+main() {
+	tput civis # hide the cursor
+	trap finish 2
+	# Main Loop
+	while true; do
+		mpd_check
+		update_art
+		clear
+		ueber_art
+	done 2>/dev/null
+}
+
+finish() {
+	# Cleanup steps
+	clear_art
+	tput cnorm # make cursor visible
+	exit 0
+}
+
 mpd_check() {
 	# wait until mpd is running
 	while true; do
@@ -19,6 +38,12 @@ mpd_check() {
 		fi
 		sleep 1
 	done
+}
+
+update_art() {
+	# update $old_filename and then (re)-fetch the filename for the current album cover
+	old_filename=$filename
+	art_filename
 }
 
 art_filename() {
@@ -43,10 +68,13 @@ art_filename() {
 	fi
 }
 
-update_art() {
-	# update $old_filename and then (re)-fetch the filename for the current album cover
-	old_filename=$filename
-	art_filename
+ueber_art() {
+	# shellcheck disable=SC2102
+	# Declare the image details, time it with the check_old function, pipe it to ImageLayer
+	{
+		ImageLayer::add [identifier]="album_art" [x]="$x" [y]="$y" [width]="$width" [height]="$height" [path]="$filename"
+		check_old
+	} | ImageLayer
 }
 
 check_old() {
@@ -60,38 +88,10 @@ check_old() {
 	done
 }
 
-ueber_art() {
-	# shellcheck disable=SC2102
-	# Declare the image details, time it with the check_old function, pipe it to ImageLayer
-	{
-		ImageLayer::add [identifier]="album_art" [x]="$x" [y]="$y" [width]="$width" [height]="$height" [path]="$filename"
-		check_old
-	} | ImageLayer
-}
-
 clear_art() {
 	# shellcheck disable=SC2102
 	{
 		ImageLayer::remove [identifier]="album_art"
 	} | ImageLayer
-}
-
-finish() {
-	# Cleanup steps
-	clear_art
-	tput cnorm # make cursor visible
-	exit 0
-}
-
-main() {
-	tput civis # hide the cursor
-	trap finish 2
-	# Main Loop
-	while true; do
-		mpd_check
-		update_art
-		clear
-		ueber_art
-	done 2>/dev/null
 }
 main
